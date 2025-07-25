3af4f533aedfdfcb03f91590cf73479e0fb0d797
kallyas <kallyasmedia@gmail.com>
2025-07-25 14:20:29 +0300

    Initial Commit

---
+++ SubTrackr/SubTrackrApp.swift
@@ -0,0 +1,32 @@
+//
+//  SubTrackrApp.swift
+//  SubTrackr
+//
+//  Created by Tumuhirwe Iden on 25/07/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SubTrackrApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ SubTrackrTests/SubTrackrTests.swift
@@ -0,0 +1,16 @@
+//
+//  SubTrackrTests.swift
+//  SubTrackrTests
+//
+//  Created by Tumuhirwe Iden on 25/07/2025.
+//
+
+import Testing
+
+struct SubTrackrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
