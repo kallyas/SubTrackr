71707c45dd1546cc88932fbf9910f61130c98463
kallyas <kallyasmedia@gmail.com>
2025-07-28 12:04:03 +0300

    Remove old data manager

--- SubTrackrWidget/WidgetDataManager.swift
+++
@@ -1,8 +0,0 @@
-//
-//  WidgetDataManager.swift
-//  SubTrackrWidget
-//
-//  Created by Tumuhirwe Iden on 25/07/2025.
-//
-//  This file will be replaced by the shared version from the main app target
-//
